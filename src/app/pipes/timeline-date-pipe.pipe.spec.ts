4622bf9c1de5f95e75daee957c9999ac8d1506ca
Danboal <www.masaya1@gmail.com>
2020-06-25 02:32:19 +0900

    タイムライン調整

---
+++ src/app/pipes/timeline-date-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TimelineDatePipePipe } from './timeline-date-pipe.pipe';
+
+describe('TimelineDatePipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new TimelineDatePipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
