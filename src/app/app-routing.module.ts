8fd2d51da8e184395bb224b4bd299f14fe8555ae
m-toda <m-toda@unixon.co.jp>
2020-07-15 11:23:27 +0900

    ログインページ作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,6 +26,10 @@ const routes: Routes = [
   {
     path: 'message',
     loadChildren: () => import('./message/message.module').then( m => m.MessagePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
